323d83e3535d4d53e69e71db0f40048eb5d15388
AthiphatNiamsot <athiphat-n@rmutp.ac.th>
2025-08-28 12:05:50 +0700

    addd

--- app/index.ts
+++ app/index.ts
@@ -1,20 +1,21 @@
 import { Hono } from "hono";
-import  {PrismaClient} from "@prisma/client";
-//import  {PrismaClient} from "../generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 const app = new Hono();
-app.get("/", (c) => c.text("Hello, word!"));
+
+app.get("/", (c) => c.text("Hello, world!"));
+
 app.get("/about", (c) => {
   return c.json({
-    Message: "Athiphat"
+    Message: "Athiphat",
   });
 });
-app.get("/profile", () => {
-   // logic
-   const profiles = prisma.Profile.findMany();
-   return c.text("profiles");
+
+app.get("/profile", async (c) => {
+  const profiles = await prisma.profile.findMany();
+  return c.json(profiles); // ส่งข้อมูลจริงออกไปเป็น JSON
 });
 
-export default app ;
+export default app;
