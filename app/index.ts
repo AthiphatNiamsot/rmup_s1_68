f60ba635cedc2ee42b5c8868ac7f57b0240565fe
AthiphatNiamsot <athiphat-n@rmutp.ac.th>
2025-09-03 17:07:53 +0700

    add

--- app/index.ts
+++ app/index.ts
@@ -1,15 +1,14 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client";
+import * as bcrypt from "bcrypt";
 
 const prisma = new PrismaClient();
-
 const app = new Hono();
 
 app.get("/", (c) => c.text("Hello, world!"));
 
 app.get("/about", (c) => {
-  return c.json({
-    Message: "Athiphat", });
+  return c.json({ Message: "Athiphat" });
 });
 
 app.get("/profile", async (c) => {               
@@ -17,11 +16,27 @@ app.get("/profile", async (c) => {
   return c.json(profile);
 });
 
-app.post("/profile" , async (c) =>  {
+app.post("/profile", async (c) => {
+
   const body = await c.req.json();
-  return c.json({Message: "Create profile complete"});
+  console.log('input of profile ', body);
+  console.log('body.password (original) ', body.password);
 
+  // encode password
+  const passwordHash = await bcrypt.hash(body.password, 10);
+  console.log('hashed password ', passwordHash);
+
+  // replace password with hashed version
+  body.password = passwordHash;
+  console.log('body (with hashed password)', body);
+
+  // save to database
+  await prisma.profile.create({ data: body });
+
+  // output response
+  return c.json({
+    message: "create profile completed",
+  });
 });
-
 
 export default app;
